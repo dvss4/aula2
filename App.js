957d5939dc09213caf9f4fab4f7bcb3b82f57a73
Dinar2020 <dinario@outlook.com.br>
2022-10-07 16:36:55 -0300

    versão incial

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button, StatusBar, SafeAreaView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+function App(){
+  const [nome, setNome] = useState('');
+  const [input, setInput] = useState('');
+  const [mostrar, setMostar] = useState(1);
+
+  function entrar(){
+    
+    if(input === ''){
+      alert('Digite o comando');
+      return;
+    }else if(input === 'html'){
+      setText()
+    }
+
+
+    setNome(input);
+
+  }
+
+
+  return(
+   <SafeAreaView style={styles.container}>
+    <StatusBar> </StatusBar>
+    <View style={{height:40, backgroundColor: 'red'}}></View>
+    <Text style={[styles.texto,{opacity: mostrar}]}> Comandos HTML</Text>
+    <TextInput
+      style={styles.input}
+      placeholder="Digite o comando"
+      onChangeText={ (texto) => setInput(texto) }
+    />
+
+    <Button title="Entrar" onPress={ entrar } />
+    <Text style={[styles.subtitulo,{opacity: mostrar}]}> Comandos disponíveis: </Text>
+    <Text style={styles.texto}> {nome} </Text>
+    <View style={{flex:1, height:80, backgroundColor: 'white'}}></View>
+    <View style={{height:40, backgroundColor: 'red'}}></View>
+   </SafeAreaView> 
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1
   },
+  input:{
+    height: 45,
+    borderWidth: 1,
+    margin: 10,
+    padding: 10,
+    fontSize: 20,
+  },
+  texto:{
+    textAlign: 'center',
+    fontSize: 25,
+    marginTop: 15
+  }
 });
+
+
+
+export default App;
